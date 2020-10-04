2101fc88df1e5696c9db3233af935469fbfa54f3
Guillermo Balmaceda <guillebalmacedaonline@gmail.com>
2020-10-04 20:04:14 -0300

    ajuste

--- resources/views/pago_exitoso.blade.php
+++ resources/views/pago_exitoso.blade.php
@@ -87,36 +87,11 @@
                             </div>
                         </div>
                         <div class="as-accessories-results  as-search-desktop">
-                            <div class="width:60%">
-                                <div class="as-producttile-tilehero with-paddlenav " style="float:left;">
-                                    <div class="as-dummy-container as-dummy-img">
-
-                                        <img src="./assets/wireless-headphones" class="ir ir item-image as-producttile-image  " style="max-width: 70%;max-height: 70%;"alt="" width="445" height="445">
-                                    </div>
-                                    <div class="images mini-gallery gal5 ">
-
-
-                                        <div class="as-isdesktop with-paddlenav with-paddlenav-onhover">
-                                            <div class="clearfix image-list xs-no-js as-util-relatedlink relatedlink" data-relatedlink="6|Powerbeats3 Wireless Earphones - Neighborhood Collection - Brick Red|MPXP2">
-                                                <div class="as-tilegallery-element as-image-selected">
-                                                    <div class=""></div>
-                                                    <img src="./assets/003.jpg" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" style="content:-webkit-image-set(url(<?php echo $_POST['img'] ?>) 2x);">
-                                                </div>
-
-                                            </div>
-
-
-                                        </div>
-
-
-
-                                    </div>
-
-                                </div>
+                            <div class="width:100%">                                
                                 <h3>PAGO EXITOSO</h3>
                                 <p> <strong>payment_method_id:</strong> </p>
                                 <p> <strong>external_reference:</strong> </p>
-                                <p> <strong>payment_id o collection_id:</strong> </p>                              
+                                <p> <strong>payment_id o collection_id:</strong> </p>
                             </div>
                         </div>
                     </div>
